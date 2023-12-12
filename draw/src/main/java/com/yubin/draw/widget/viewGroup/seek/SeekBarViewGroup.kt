517c575c81040e20a4d4b91e7d27bf95d302093f
DingYubin <ubivedyb@163.com>
2023-12-12 11:46:20 +0800

    完成距离控件

--- draw/src/main/java/com/yubin/draw/widget/viewGroup/seek/SeekBarViewGroup.kt
+++ draw/src/main/java/com/yubin/draw/widget/viewGroup/seek/SeekBarViewGroup.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.yubin.baselibrary.util.CMDisplayHelper.dp
+import com.yubin.baselibrary.util.LogUtil
 import com.yubin.draw.R
 
 class SeekBarViewGroup : ConstraintLayout {
@@ -40,36 +41,43 @@ class SeekBarViewGroup : ConstraintLayout {
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                if (progress == seekBar.min) {
-                    indicator.visibility = GONE
-                } else {
-                    indicator.visibility = VISIBLE
-                }
+                post {
 
-                //进度百分比
-                val progressRatio = progress.toFloat() / seekBar.max
-                //文本宽度
-                val indicatorW = indicator.measuredWidth / 2
-                // thumb偏移量
-                val thumbOffset = seekBar.thumbOffset
-                // 拖动图片宽度
-                val thumbW = seekBar.thumb.intrinsicWidth / 2
-                val with = seekBar.measuredWidth - thumbW - thumbOffset - 44.dp
-                val s = seekBar.measuredWidth - (progressRatio * seekBar.measuredWidth)
-                val leftMargin = if (s >= indicator.measuredWidth) {
-                    val left = (progress * with) / seekBar.max + indicatorW + thumbW
-                    if (left < 0) 0 else left
-                } else {
-                    val left = (progress * with) / seekBar.max - indicatorW - thumbW
-                    if (left < 0) 0 else left
-                }
+                    LogUtil.d("onProgressChanged")
+                    LogUtil.d("indicator.measuredWidth : ${indicator.measuredWidth}")
+                    LogUtil.d("seekBar.measuredWidth : ${seekBar.measuredWidth}")
+                    //进度百分比
+                    val progressRatio = progress.toFloat() / seekBar.max
+                    //文本宽度
+                    val indicatorW = indicator.measuredWidth / 2
+                    // thumb偏移量
+                    val thumbOffset = seekBar.thumbOffset
+                    // 拖动图片宽度
+                    val thumbW = seekBar.thumb.intrinsicWidth / 2
+                    val with = seekBar.measuredWidth - thumbW - thumbOffset - 44.dp
+                    val s = seekBar.measuredWidth - (progressRatio * seekBar.measuredWidth)
+                    val leftMargin = if (s >= indicator.measuredWidth) {
+                        val left = (progress * with) / seekBar.max + indicatorW + thumbW
+                        if (left < 0) 0 else left
+                    } else {
+                        val left = (progress * with) / seekBar.max - indicatorW - thumbW
+                        if (left < 0) 0 else left
+                    }
 
-                val params = indicator.layoutParams as LayoutParams
-                params.leftMargin = leftMargin
-                indicator.layoutParams = params
+                    val params = indicator.layoutParams as LayoutParams
+                    params.leftMargin = leftMargin
+                    indicator.layoutParams = params
 //                val tv = "$progress%"
 //                indicator.text = tv
 //                hourlyLayout.addView(tag)
+                    if (progress == seekBar.min) {
+                        indicator.visibility = INVISIBLE
+                    } else {
+                        indicator.visibility = VISIBLE
+                    }
+
+                }
+
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -78,6 +86,13 @@ class SeekBarViewGroup : ConstraintLayout {
             override fun onStopTrackingTouch(seekBar: SeekBar) {
             }
         })
+
+
+//        seekBar.progress = 20
+    }
+
+    fun setProgress(progress: Int) {
+        seekBar.progress = progress
     }
 
 }
